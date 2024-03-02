2cb5c2859ab7928fab6bc9d55f037b7bfdf31b91
Azim <flavers001@gmail.com>
2024-03-02 18:56:13 +0600

    -swagger

--- src/main/java/org/example/springexam/SpringExamApplication.java
+++ src/main/java/org/example/springexam/SpringExamApplication.java
@@ -2,8 +2,11 @@ package org.example.springexam;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-@SpringBootApplication
+@EnableScheduling
+
 public class SpringExamApplication {
 
     public static void main(String[] args) {
--- src/main/java/org/example/springexam/controller/ParkingSpaceController.java
+++ src/main/java/org/example/springexam/controller/ParkingSpaceController.java
@@ -1,6 +1,8 @@
 package org.example.springexam.controller;
 
 
+import io.swagger.annotations.ApiOperation;
+import io.swagger.v3.oas.annotations.Operation;
 import org.example.springexam.dto.ParkingSpaceDto;
 import org.example.springexam.dto.ReservationRequestDto;
 import org.example.springexam.enums.ParkingSpaceType;
@@ -19,30 +21,31 @@ public class ParkingSpaceController {
     @Autowired
     private ParkingSpaceService parkingSpaceService;
 
+@ApiOperation("GET")
     @GetMapping("/spaces")
     public ResponseEntity<List<ParkingSpaceDto>> getAllParkingSpaces() {
         List<ParkingSpaceDto> parkingSpaces = parkingSpaceService.getAllParkingSpaces();
         return ResponseEntity.ok(parkingSpaces);
     }
-
+    @ApiOperation("GET")
     @GetMapping("/spaces/{id}")
     public ResponseEntity<ParkingSpaceDto> getParkingSpaceById(@PathVariable Long id) {
         Optional<ParkingSpaceDto> parkingSpaceDto = parkingSpaceService.getParkingSpaceById(id);
         return parkingSpaceDto.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
     }
-
+    @ApiOperation("POST")
     @PostMapping("/reserve")
     public ResponseEntity<ParkingSpaceDto> reserveParkingSpace(@RequestBody ReservationRequestDto reservationRequestDto) {
         ParkingSpaceDto reservedSpace = parkingSpaceService.reserveParkingSpace(reservationRequestDto.getParkingSpaceId());
         return reservedSpace != null ? ResponseEntity.ok(reservedSpace) : ResponseEntity.badRequest().build();
     }
-
+    @ApiOperation("PUT")
     @PutMapping("/release/{id}")
     public ResponseEntity<ParkingSpaceDto> releaseParkingSpace(@PathVariable Long id) {
         ParkingSpaceDto releasedSpace = parkingSpaceService.releaseParkingSpace(id);
         return releasedSpace != null ? ResponseEntity.ok(releasedSpace) : ResponseEntity.badRequest().build();
     }
-
+    @ApiOperation("GET")
     @GetMapping("/spaces/type/{type}")
     public ResponseEntity<List<ParkingSpaceDto>> getParkingSpacesByType(@PathVariable ParkingSpaceType type) {
         List<ParkingSpaceDto> parkingSpaces = parkingSpaceService.getParkingSpacesByType(type);
